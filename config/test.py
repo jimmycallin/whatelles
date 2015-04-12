4cd46fda82d46930d9c2c488930d2f65bc8acfea
Jimmy Callin <jimmy.callin@gmail.com>
2015-04-12 19:16:55 +0200

    setting up configuration files

---
+++ config/test.py
@@ -0,0 +1,7 @@
+def get_configs():
+    config = {
+        'project_name': 'PronounTest',
+        'train_path': '/Users/jimmy/dev/edu/mt/pronoun/resources/train/devset',
+        'test_path': '/Users/jimmy/dev/edu/mt/pronoun/resources/test/teddev'
+    }
+    return [config]
