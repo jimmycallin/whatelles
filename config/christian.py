ccdfe8ebb7a0cd9ee65bfed4373becd633ccf148
Jimmy Callin <jimmy.callin@gmail.com>
2015-05-28 16:29:58 +0200

    christian

--- config/christian.py
+++ config/christian.py
@@ -3,10 +3,16 @@ from model import cross_entropy
 from model import PronounPrediction
 
 
+"""
+This configuration tries to be as similar to Hardmeier's version as possible.
+That said, it's really not that similar at all. I'll try to update the code soon.
+"""
+
+
 def get_configs():
     config = {'model': PronounPrediction,
               'n_hiddens': [50],
-              'embedding_dimensionality': 20,  # look into dimensionality more later. 50 worked well.
+              'embedding_dimensionality': 20,
               'activation_function': T.nnet.sigmoid,
               'cost_function': cross_entropy,
               'n_epochs': 1000,
@@ -16,6 +22,7 @@ def get_configs():
               'classes_filepath': 'resources/train/ncv9/classes.csv',
               'training_filepath': 'resources/train/ncv9/data.csv',
               'development_filepath': 'resources/train/devset_larger/data.csv',
-              'test_filepath': 'resources/test/teddev/data.csv'}
+              'test_filepath': 'resources/test/teddev/data.csv',
+              'n_tags': 5}
 
     return [config]
