8e8f15a21a9af450ef40af49ac9547f3f5b20c06
Jimmy Callin <jimmy.callin@gmail.com>
2015-05-28 16:30:46 +0200

    updates

--- tools/doc-starts-at.py
+++ tools/doc-starts-at.py
@@ -1,10 +1,11 @@
+from sys import argv
 """
 Creates document id files where each row id represents
 where a document starts in each corpus. This is necessary
 for running mmax converter.
 """
 
-with open('doc-ids.csv') as f:
+with open(argv[1]) as f:
     with open('doc-starts-at.csv', 'w') as w:
         docid = "-1"
         for i, did in enumerate(f):
